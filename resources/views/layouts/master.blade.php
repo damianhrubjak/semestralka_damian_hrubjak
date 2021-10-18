295c4c858a14915893bdb5584c3cb19a373d3800
Damián Hrubják <hrubjak.damian@gmail.com>
2021-10-18 17:02:56 +0200

    Initial commit; email -> username; Tailwind, SCSS

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>@yield('title')</title>
+
+        <meta name="author" content="Damián Hrubják" />
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap"
+            rel="stylesheet">
+
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}" />
+
+    </head>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <div class="js-scripts">
+        <script defer src="{{ mix('js/app.js') }}"></script>
+    </div>
+    </body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+        "./resources/**/*.vue",
+    ],
+    mode: "jit",
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
